38d45118689f9913746c9e645aba3abe15157717
nedjwab <62156607+nedjwab@users.noreply.github.com>
2022-06-03 11:51:43 +0100

    Add show items to the

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 import './style.css';
+import getData from './modules/getdata.js';
+
+
+document.addEventListener('DOMContentLoaded', showMovies);
+
+const displayItem = async () => {
+  const movies = await getData();
+  showMovies(movies);
+};
+displayItem();
